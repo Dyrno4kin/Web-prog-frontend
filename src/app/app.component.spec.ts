eaaa0edf4c65cc6c74c09cd990841ecdbe8344bd
Dyrno4kin <dyrno4kin@gmail.com>
2019-09-25 09:58:21 +0400

    Create_lab_2

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -32,4 +32,6 @@ describe('AppComponent', () => {
     const compiled = fixture.debugElement.nativeElement;
     expect(compiled.querySelector('.content span').textContent).toContain('dyrno4kin-app app is running!');
   });
+
+  
 });
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LandingComponent } from './landing/landing.component';
+import { MaterialsComponent } from './materials/materials.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LandingComponent,
+    MaterialsComponent
   ],
   imports: [
     BrowserModule,
