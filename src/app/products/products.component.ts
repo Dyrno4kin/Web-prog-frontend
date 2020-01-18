7d2590564a189fa8e36c52acc7743cf8499eccf6
Dyrno4kin <dyrno4kin@gmail.com>
2020-01-18 12:24:51 +0400

    lab 10

--- src/app/products/products.component.ts
+++ src/app/products/products.component.ts
@@ -3,7 +3,8 @@ import { NgForm } from '@angular/forms'
 import { ProductService } from 'src/app/products/shared/product.service'
 import { ToastrService } from 'ngx-toastr';
 import { ProductListComponent } from './product-list/product-list.component';
-
+import { HttpClient } from '@angular/common/http';
+import { HttpHeaders } from '@angular/common/http';
 
 @Component({
   selector: 'app-products',
@@ -12,13 +13,13 @@ import { ProductListComponent } from './product-list/product-list.component';
 })
 export class ProductsComponent implements OnInit {
 
-  @ViewChild(ProductListComponent, {static: false}) list: ProductListComponent;
+  @ViewChild(ProductListComponent, { static: false }) list: ProductListComponent;
 
-  constructor(private productService: ProductService, private toastr: ToastrService){
+  constructor(private productService: ProductService, private toastr: ToastrService, private http: HttpClient) {
     this.ws.onopen = () => {
       this.setStatus('ONLINE');
       this.ws.onmessage = (response) => {
-          this.toastr.success(response.data);
+        this.toastr.success(response.data);
         this.printMessage(response.data);
       };
     };
@@ -30,18 +31,25 @@ export class ProductsComponent implements OnInit {
     console.log(value)
   }
   printMessage(value) {
-  //  this.toastr.success('User login successful');
+    //  this.toastr.success('User login successful');
     console.log(value);
   }
   SendMessage() {
     console.log("I'm is Admin and i send message!");
-    var te = new TextEncoder();
-    this.ws.send(this.productService.selectedProduct.ProductName); 
-    //this.ws.send('isUpgrade');
+    this.ws.send('isUpgrade');
   }
 
-  
+
   ngOnInit() {
+    this.toastr.success("до запроса")
+    const httpOptions = { headers: new HttpHeaders({ 'Content-Type': 'application/json' }) };
+    const data: any = {
+      'url': 'ws://localhost:3000'
+    };
+    this.http.post("http://localhost:3100/subscribe", <JSON>data, httpOptions).subscribe(data => {
+      console.log(data);
+    });
+    console.log("после запроса2")
     this.resetForm();
   }
 
@@ -59,25 +67,25 @@ export class ProductsComponent implements OnInit {
   }
 
   onSubmit(form: NgForm) {
-     
-   if (form.value.id == null) {
-    this.productService.postProduct(form.value)
-      .subscribe(data => {
-        this.resetForm(form);
-        this.toastr.success('Запись добавлена!', 'Products');
-        this.list.ngOnInit();
-      })
-  }
-  else {
-    this.productService.putProduct(form.value.id, form.value)
-    .subscribe(data => {
-      this.resetForm(form);
-      this.list.ngOnInit();
-      this.toastr.success('Запись изменена!', 'Products');
-    });
-  }
+
+    if (form.value.id == null) {
+      this.productService.postProduct(form.value)
+        .subscribe(data => {
+          this.resetForm(form);
+          this.toastr.success('Запись добавлена!', 'Products');
+          this.list.ngOnInit();
+        })
+    }
+    else {
+      this.productService.putProduct(form.value.id, form.value)
+        .subscribe(data => {
+          this.resetForm(form);
+          this.list.ngOnInit();
+          this.toastr.success('Запись изменена!', 'Products');
+        });
+    }
   }
 
-  
-  
+
+
 }
